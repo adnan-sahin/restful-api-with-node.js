00e890d2f81a87be987d617e244f8d743fe3bfb7
adnan-sahin <adnansahin1@outlook.com>
2019-02-03 11:08:46 +0300

    put, patch, delete and getById methods were moved to controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -21,11 +21,69 @@ function booksController(Book) {
         if (err) {
           return res.send(err);
         }
-        return res.json(books);
+        const returnBooks = books.map((book) => {
+          const newBook = book.toJSON();
+          newBook.links = {};
+          /* eslint-disable-next-line no-underscore-dangle */
+          newBook.links.self = `http://${req.headers.host}/api/books/${book._id}`;
+          return newBook;
+        });
+
+
+        return res.json(returnBooks);
       }
     );
   }
-  return { post, get };
+  function getById(req, res) {
+    const returnBook = req.book.toJSON();
+    returnBook.links = {};
+    const genre = req.book.genre.replace(' ', '%20');
+    returnBook.links.FilterByThisGenre = `http://${req.headers.host}/api/books?genre=${genre}`;
+    res.json(returnBook);
+  }
+  function put(req, res) {
+    const { book } = req;
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.genre = req.body.genre;
+    book.read = req.body.read;
+    req.book.save((err) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
+  }
+  function patch(req, res) {
+    const { book } = req;
+    /* eslint-disable-next-line no-underscore-dangle */
+    if (req.body._id) {
+      /* eslint-disable-next-line no-underscore-dangle */
+      delete req.body._id;
+    }
+    Object.entries(req.body).forEach((item) => {
+      const key = item[0];
+      const value = item[1];
+      book[key] = value;
+    });
+    req.book.save((err) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
+  }
+  function deleteBook(req, res) {
+    req.book.remove((err) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.sendStatus(204);
+    });
+  }
+  return {
+    get, getById, post, put, patch, deleteBook
+  };
 }
 
 module.exports = booksController;
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -23,47 +23,10 @@ function routes(Book) {
   });
 
   bookRouter.route('/books/:bookId')
-    .get((req, res) => res.json(req.book))
-    .put((req, res) => {
-      const { book } = req;
-      book.title = req.body.title;
-      book.author = req.body.author;
-      book.genre = req.body.genre;
-      book.read = req.body.read;
-      req.book.save((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(book);
-      });
-    })
-    .patch((req, res) => {
-      const { book } = req;
-      /* eslint-disable-next-line no-underscore-dangle */
-      if (req.body._id) {
-        /* eslint-disable-next-line no-underscore-dangle */
-        delete req.body._id;
-      }
-      Object.entries(req.body).forEach((item) => {
-        const key = item[0];
-        const value = item[1];
-        book[key] = value;
-      });
-      req.book.save((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(book);
-      });
-    })
-    .delete((req, res) => {
-      req.book.remove((err) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.sendStatus(204);
-      });
-    });
+    .get(controller.getById)
+    .put(controller.put)
+    .patch(controller.patch)
+    .delete(controller.deleteBook);
 
   return bookRouter;
 }
